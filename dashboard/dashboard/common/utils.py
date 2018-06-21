a43a35cf5af244cc92baede522cf6f87fe297de1
Simon <simonhatch@chromium.org>
2018-06-20 21:01:57 -0400

    Dashboard - Use isinstance() instead of type()
    TBR=eakuefner@chromium.org
    Change-Id: If8aa6d637e764d0589101f194582134487eda805
    Reviewed-on: https://chromium-review.googlesource.com/1109389
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: Simon Hatch <simonhatch@chromium.org>

--- dashboard/dashboard/common/utils.py
+++ dashboard/dashboard/common/utils.py
@@ -185,7 +185,7 @@ def MostSpecificMatchingPattern(test, pattern_data_tuples):
   if not matching_patterns:
     return None
 
-  if type(test) is ndb.Key:
+  if isinstance(test, ndb.Key):
     test_path = TestPath(test)
   else:
     test_path = test.test_path
@@ -228,7 +228,7 @@ def TestMatchesPattern(test, pattern):
   """
   if not test:
     return False
-  if type(test) is ndb.Key:
+  if isinstance(test, ndb.Key):
     test_path = TestPath(test)
   else:
     test_path = test.test_path
@@ -279,7 +279,7 @@ def GetTestContainerKey(test):
     ndb.Key('TestContainer', test path)
   """
   test_path = None
-  if type(test) is ndb.Key:
+  if isinstance(test, ndb.Key):
     test_path = TestPath(test)
   else:
     test_path = test.test_path
@@ -489,7 +489,7 @@ def Validate(expected, actual):
     actual: A value.
   """
   def IsValidType(expected, actual):
-    if type(expected) is type and type(actual) is not expected:
+    if isinstance(expected, type) and not isinstance(actual, expected):
       try:
         expected(actual)
       except ValueError:
