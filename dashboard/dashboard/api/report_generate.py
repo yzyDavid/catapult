6947a764dbc57e1047dc96854c5b7f14646477ce
Ben Hayden <benjhayden@chromium.org>
2018-07-29 14:18:41 -0700

    Gradually merging master.

---
+++ dashboard/dashboard/api/report_generate.py
@@ -0,0 +1,45 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from dashboard.api import api_request_handler
+from dashboard.models import report_template
+
+
+CACHE_SECONDS = 60 * 60 * 20
+
+
+class ReportGenerateHandler(api_request_handler.ApiRequestHandler):
+
+  def get(self):
+    self.post()
+
+  def _AllowAnonymous(self):
+    return True
+
+  def AuthorizedPost(self):
+    revisions = self.request.get('revisions', None)
+    if revisions is None:
+      raise api_request_handler.BadRequestError
+    try:
+      revisions = [int(r) if r != 'latest' else r
+                   for r in revisions.split(',')]
+    except ValueError:
+      raise api_request_handler.BadRequestError
+
+    try:
+      template_id = int(self.request.get('id'))
+    except ValueError:
+      raise api_request_handler.BadRequestError
+
+    try:
+      report = report_template.GetReport(template_id, revisions)
+    except AssertionError:
+      # The caller has requested internal-only data but is not authorized.
+      raise api_request_handler.NotFoundError
+    if report is None:
+      raise api_request_handler.NotFoundError
+
+    self.response.headers['Cache-Control'] = '%s, max-age=%d' % (
+        'private' if report['internal'] else 'public', CACHE_SECONDS)
+    return report
