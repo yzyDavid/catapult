9be5f240550fb414f764814ac87d4ea44132c81b
Ben Hayden <benjhayden@chromium.org>
2018-09-19 14:13:26 -0700

    Manually merge master

--- dashboard/dashboard/update_dashboard_stats.py
+++ dashboard/dashboard/update_dashboard_stats.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 
 import datetime
+import httplib
 import time
 
 from google.appengine.ext import deferred
@@ -11,6 +12,7 @@ from google.appengine.ext import ndb
 from dashboard import add_histograms
 from dashboard.common import datastore_hooks
 from dashboard.common import request_handler
+from dashboard.common import utils
 from dashboard.models import anomaly
 from dashboard.pinpoint.models import job as job_module
 from dashboard.pinpoint.models import job_state
@@ -86,8 +88,8 @@ def _CreateHistogramSet(
   return histograms
 
 
-def _CreateHistogram(name, story=None):
-  h = histogram_module.Histogram(name, 'msBestFitFormat')
+def _CreateHistogram(name, unit, story=None):
+  h = histogram_module.Histogram(name, unit)
   if story:
     h.diagnostics[reserved_infos.STORIES.name] = (
         generic_set.GenericSet([story]))
@@ -96,12 +98,14 @@ def _CreateHistogram(name, story=None):
 
 def _GetDiffCommitTimeFromJob(job):
   diffs = job.state.Differences()
-  for d in diffs:
-    diff = d[1].AsDict()
-    commit_time = datetime.datetime.strptime(
-        diff['commits'][0]['time'], '%a %b %d %X %Y')
-    return commit_time
-  return None
+  try:
+    for d in diffs:
+      diff = d[1].AsDict()
+      commit_time = datetime.datetime.strptime(
+          diff['commits'][0]['time'], '%a %b %d %X %Y')
+      return commit_time
+  except httplib.HTTPException:
+    return None
 
 
 @ndb.tasklet
@@ -137,24 +141,75 @@ def _FetchStatsForJob(job, commit_time):
       time_from_job_to_culprit))
 
 
+@ndb.synctasklet
 def _FetchDashboardStats():
+  process_alerts_future = _ProcessAlerts()
+
   completed_jobs = _FetchCompletedPinpointJobs(
       datetime.datetime.now() - datetime.timedelta(days=14))
 
-  _ProcessPinpointJobs(completed_jobs)
+  yield [
+      _ProcessPinpointJobs(completed_jobs),
+      process_alerts_future]
 
 
-@ndb.synctasklet
+@ndb.tasklet
+def _ProcessAlerts():
+  sheriff = ndb.Key('Sheriff', 'Chromium Perf Sheriff')
+  ts_start = datetime.datetime.now() - datetime.timedelta(days=1)
+
+  q = anomaly.Anomaly.query()
+  q = q.filter(anomaly.Anomaly.timestamp > ts_start)
+  q = q.filter(anomaly.Anomaly.sheriff == sheriff)
+  q = q.order(-anomaly.Anomaly.timestamp)
+
+  alerts = yield q.fetch_async()
+  if not alerts:
+    raise ndb.Return()
+
+  alerts_total = _CreateHistogram('chromium.perf.alerts', 'count')
+  alerts_total.AddSample(len(alerts))
+
+  count_by_suite = {}
+
+  for a in alerts:
+    test_suite_name = utils.TestSuiteName(a.test)
+    if test_suite_name not in count_by_suite:
+      count_by_suite[test_suite_name] = 0
+    count_by_suite[test_suite_name] += 1
+
+  hists_by_suite = {}
+  for s, c in count_by_suite.iteritems():
+    hists_by_suite[s] = _CreateHistogram(
+        'chromium.perf.alerts', 'count', story=s)
+    hists_by_suite[s].AddSample(c)
+
+  hs = _CreateHistogramSet(
+      'ChromiumPerfFyi', 'test1', 'chromeperf.stats', int(time.time()),
+      [alerts_total] + hists_by_suite.values())
+
+  deferred.defer(
+      add_histograms.ProcessHistogramSet, hs.AsDicts())
+
+
+@ndb.tasklet
 def _ProcessPinpointJobs(jobs_and_commits):
   job_results = yield [_FetchStatsForJob(j, c) for j, c in jobs_and_commits]
   job_results = [j for j in job_results if j]
   if not job_results:
     raise ndb.Return(None)
 
-  commit_to_culprit = _CreateHistogram('pinpoint')
-  commit_to_alert = _CreateHistogram('pinpoint', story='commitToAlert')
-  alert_to_job = _CreateHistogram('pinpoint', story='alertToJob')
-  job_to_culprit = _CreateHistogram('pinpoint', story='jobToCulprit')
+  commit_to_culprit = _CreateHistogram('pinpoint', 'msBestFitFormat')
+  commit_to_culprit.CustomizeSummaryOptions({
+      'percentile': [0.5, 0.9]
+  })
+
+  commit_to_alert = _CreateHistogram(
+      'pinpoint', 'msBestFitFormat', story='commitToAlert')
+  alert_to_job = _CreateHistogram(
+      'pinpoint', 'msBestFitFormat', story='alertToJob')
+  job_to_culprit = _CreateHistogram(
+      'pinpoint', 'msBestFitFormat', story='jobToCulprit')
 
   for result in job_results:
     time_from_land_to_culprit = result[0]
